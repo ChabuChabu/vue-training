ca3e92d5a9ce329d4f8a93c62cdae4869534d45b
ChabuChabu <chabuchabu32@gmail.cpm>
2022-11-28 13:10:32 +0000

    model project complete

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,6 +1,25 @@
 <template>
    <h1>{{ title }}</h1>
-   <Modal />
+  <div v-if="showModal">
+    <Modal  theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">Sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Giveaway</h1>
+        <p>Grab your ninja swag for half the price!!</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+        <h1>Sign up to the newsletter</h1>
+        <p>For updates and promo codes!!</p>
+    </Modal>
+  </div>
+  <p>Welcome ...</p>
+  <button @click="toggleModal">open modal(shift)</button>
+  <button @click="toggleModalTwo">open modal two</button>
 </template>
 
 <script>
@@ -11,7 +30,17 @@ export default {
   components: { Modal },
   data(){
     return{
-      title:'Frontend Engineer Architecture using Vue.js'
+      title:'Frontend Engineering Architecture using Vue.js',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods:{
+    toggleModal(){
+      this.showModal = ! this.showModal 
+    },
+    toggleModalTwo(){
+      this.showModalTwo = ! this.showModalTwo 
     }
   }
 }
